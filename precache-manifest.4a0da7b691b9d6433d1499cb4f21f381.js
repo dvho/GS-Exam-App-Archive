90204beff187cbe29bb8ad4f85c61d4eb3cb8b41
dvho <email6@gmail.com>
2022-03-30 00:59:43 -0400

    Updates

---
+++ precache-manifest.4a0da7b691b9d6433d1499cb4f21f381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f95d5749ef630d62c5d9b578af1036",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3648d1fce2e40b3e3d9",
+    "url": "./static/css/main.a5c24714.chunk.css"
+  },
+  {
+    "revision": "a352006402f81b06f955",
+    "url": "./static/js/2.619ff36a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.619ff36a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3648d1fce2e40b3e3d9",
+    "url": "./static/js/main.39898eac.chunk.js"
+  },
+  {
+    "revision": "0d9d59c45289ff198f1b",
+    "url": "./static/js/runtime-main.d74a70d5.js"
+  }
+]);
